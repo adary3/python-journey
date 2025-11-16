019a1104cb9db10306af7850a278d91da9dfa7a2
a_dary33 <a_dary33@outlook.com>
2025-11-16 12:45:52 +0200

    Day 1: First script - variables, input, math

---
+++ day1-basics.py
@@ -0,0 +1,28 @@
+# Python Basics - @a_dary33
+
+# 1. Print My name
+
+print("I am @a_dary33. I am Python.")
+
+# 2. Variables (store data)
+name = "a_dary33"
+age = 23
+country = "Australia"
+dream = "Be excellent coder on this galaxy"
+
+# 3. Print With variables
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Country: {country}")
+print(f"Dream: {dream}")
+
+# 4. Input (talk to the your program)
+user_dream = input("What is your dream?")
+print(f"You said: {user_dream}")
+
+# 5. Math (simple calculatior)
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(f"Sum {num1 + num2}")
+print(f"Product; {num1 * num2}")
+
